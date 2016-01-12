ac2a8fc8e0bfa4c948723767ff3b8187456a8d72
Houston S. Fernandes <houstonfernandes@gmail.com>
2016-01-12 13:18:37 -0200

    rotas alteradas para exercicio 3 prefixo admin/

--- app/Http/Controllers/AdminCategoriesController.php
+++ app/Http/Controllers/AdminCategoriesController.php
@@ -21,69 +21,43 @@ class AdminCategoriesController extends Controller
         return view('category_list', compact('categories'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        echo"create category";
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function retrieve($id)
     {
-        //
+        try{
+            if($id==null)
+                throw new Exception('Id não informado!');
+            echo 'retrieve'.$id . "<br>";
+            echo $this->categories->find($id)->name;
+        }
+        catch( Exception $e){
+            echo $e->getMessage();
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function update($id)
     {
-        //
+        try {
+            if ($id == null)
+                throw new Exception('Id não informado!');
+            echo 'update' . $id;
+
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function delete($id)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-}
+        try {
+            if ($id == null)
+                throw new Exception('Id não informado!');
+            echo 'delete' . $id . "<br>";
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,19 +10,34 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-
+Route::pattern('id','[0-9]+');
+Route::group(['prefix'=>'admin'],function(){
+    Route::group(['prefix'=>'products'], function(){
+        get('/', ['as' => 'product_list', 'uses' => 'AdminProductsController@index']);
+        get('create', ['as' => 'product_create', 'uses' => 'AdminProductsController@create']);
+        get('retrieve/{id}',['as' => 'product_retrieve', 'uses' => 'AdminProductsController@retrieve']);
+        get('update/{id}', ['as' => 'product_update', 'uses' => 'AdminProductsController@update']);
+        get('delete/{id}', ['as' => 'product_delete', 'uses' => 'AdminProductsController@delete']);
+    });
+    Route::group(['prefix'=>'categories'], function(){
+        get('/', ['as' => 'categories_list', 'uses' => 'AdminCategoriesController@index']);
+        get('create', ['as' => 'categories_create', 'uses' => 'AdminCategoriesController@create']);
+        get('retrieve/{id}',['as' => 'categories_retrieve', 'uses' => 'AdminCategoriesController@retrieve']);
+        get('update/{id}', ['as' => 'categories_update', 'uses' => 'AdminCategoriesController@update']);
+        get('delete/{id}', ['as' => 'categories_delete', 'uses' => 'AdminCategoriesController@delete']);
+    });
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//meus testes
+/*/meus testes
 Route::get('welcome', 'Welcome@index');
 Route::get('category', 'CategoryController@listar');
-
+/* ex2
 // admin/categories: Deve apontar para o controller AdminCategoriesController e para action index
 Route::get('admin/categories', 'AdminCategoriesController@index');
 // admin/products: Deve apontar para o controller AdminProductsController e para action index
 Route::get('admin/products', 'AdminProductsController@index');
-
+*/
